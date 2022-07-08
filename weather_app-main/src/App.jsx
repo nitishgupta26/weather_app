d3f9e870be3e420504da515a4fb14c4a9aaae308
nitishgupta26 <nitishkhandelwal2611@gmail.com>
2022-07-08 17:15:33 +0530

    version1

---
+++ weather_app-main/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Tempapp from './Components/Tempapp';
+const App = () => {
+    return (
+        <>
+            <Tempapp />
+        </>
+    );
+}
+export default App;
